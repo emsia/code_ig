aefa244e184dbd5d5af003124304b31aa4d3833f
emsia <esia.rizal@gmail.com>
2013-10-10 03:31:38 -0700

    all

--- application/views/evaluation/sidebar.php
+++ application/views/evaluation/sidebar.php
@@ -1,17 +1,6 @@
 <div class="span3">
-	<div class="col-md-4">
 	<div class="todo">
-		<ul>
-			<li>
-                <div class="todo-icon fui-list"></div>
-                <div class="todo-content">
-                  <h4 class="todo-name">
-                    Chat with <strong>V.Kudinov</strong>
-                  </h4>
-                  Skype conference an 9 am
-                </div>
-              </li>
-			  <?php $active_nav = 'DASHBOARD'?>
+		<ul>			
 			<li <?php if (  !empty($active_nav) && $active_nav == 'DASHBOARD' ){?>class="todo-done" <?php } ?> >
 				<a style="text-decoration: none" href="{% url 'classes:dashboard' %}">
 					<div class="todo-icon fui-cmd"></div>
@@ -36,8 +25,8 @@
 
 			<li <?php if(   !empty($active_nav) && $active_nav ==  'EXAMS'){ ?>class="todo-done" <?php }?> >
 				<a style="text-decoration: none" href="{% url 'essays:list' %}">
-					<div class="sidebar-icon fui-location"></div>
 					<div class="todo-content">
+					<div class="todo-icon fui-location"></div>
 						<h4 class="todo-name">
 							COMMENTS *
 						</h4>
@@ -45,6 +34,5 @@
 				</a>
 			</li>
 		</ul>
-	</div>
 	</div>
 </div>
